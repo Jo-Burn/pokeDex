dbfbeec8c71f0bfcd3dac1e55def422d133f0b2a
Josiah Burnette <josiahburnette02@gmail.com>
2025-01-11 15:09:30 -0500

    Update pokeDex.js
    Got the API to work, now trying to get it to display everything

--- pokeDex.js
+++ pokeDex.js
@@ -1,10 +1,11 @@
 let input = document.getElementById('search-input');
 let click = document.getElementById('search-button');
-let name = document.getElementById('pokemon-name');
+let pokeName = document.getElementById('pokemon-name');
 let pokeId = document.getElementById('pokemon-id');
 let weight = document.getElementById('weight');
 let height = document.getElementById('height');
 let type = document.getElementById('types');
+
 let hp = document.getElementById('hp');
 let attack = document.getElementById('attack');
 let defense = document.getElementById('defense');
@@ -30,11 +31,40 @@ async function getData(a) {
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
-  
+      
       const json = await response.json();
       console.log(json);
+     statMaster(json)
+     // console.log(json.headers)
+      
     } catch (error) {
       console.error(error.message);
     } 
   }
  // getData()
+ function statMaster(a) {
+  let statSpread = [
+    ["hp", document.getElementById('hp')],
+    ["attack", document.getElementById('attack')],
+    ["defense", document.getElementById('defense')],
+    ["special-attack", document.getElementById('special-attack')],
+    ["special-defense", document.getElementById('special-defense')],
+    ["speed", document.getElementById('speed')]
+  ]
+  const { name } = a;
+  const test = name;
+  pokeName.innerHTML = `Name: ${name}`
+  const temp = (a) => {    
+    const { stats } = a;
+    let i = 0;
+    while(i < stats.length) {
+      const out = stats[i];
+      const { base_stat } = out;
+      console.log(base_stat)
+      statSpread[i][1].innerHTML = `${statSpread[i][0]}: ${base_stat}`
+      i++
+    }
+    
+  }
+  temp(a)
+ }
